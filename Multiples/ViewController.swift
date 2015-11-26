4b40792822a6aed23e4580833a8aa58e427b5fac
Christopher Augg <chrisa@christopheraugg.com>
2015-11-25 21:43:08 -0800

    Added multiples project files to repository

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -8,18 +8,101 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    // IBOutlets
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var addButtonBgImage: UIButton!
+    @IBOutlet weak var playButtonBgImage: UIButton!
+    @IBOutlet weak var multipleTxtField: UITextField!
+    @IBOutlet weak var evaluationLabel: UILabel!
+    
+    // Properties
+    var numA = 0
+    var numB = 0
+    
+    // Constant
+    let maxSum = 1000
+    
+    // Viewcontroller methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.multipleTxtField.delegate = self;
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Allowing the keyboard to minimize when return key is pressed
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
+    
+    
+    
+    
+    // IBActions
+    @IBAction func playButtonPressed() {
+        
+        
+        if let possibleNumber = Int(multipleTxtField.text!) {
+            numA = possibleNumber
+            startMultiples(true, sectionB: false)
+        } else {
+            let alertController = UIAlertController(title: "Validation Error", message: "You must enter an integer number!", preferredStyle: .Alert)
+            let alertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Destructive, handler: {(alert : UIAlertAction!) in
+                alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertController.addAction(alertAction)
+            presentViewController(alertController, animated: true, completion: nil)
+            
+           
+        }
 
+       
+        
+    }
+    
+    @IBAction func addButtonPressed() {
+        
+        let number = sum(numA, numY: numB)
+        evaluationLabel.text = "\(numB) + \(numA) = \(number)"
+        numB = number
+        
+        if isGameOver() {
+            numA = 0
+            numB = 0
+            // reset text
+            multipleTxtField.text = ""
+            evaluationLabel.text = "Press Add to Add!"
+            startMultiples(false, sectionB: true)
+        }
+        
+    }
+    
+    // functions
+    
+    func sum(numX: Int, numY: Int) -> Int {
+        return numX + numY
+    }
+    
+    func isGameOver() -> Bool {
+        return numB >= maxSum
+    }
+    
+    
+    func startMultiples(sectionA: Bool, sectionB: Bool) {
+        
+        logoImage.hidden = sectionA
+        multipleTxtField.hidden = sectionA
+        playButtonBgImage.hidden = sectionA
+        
+        addButtonBgImage.hidden = sectionB
+        evaluationLabel.hidden = sectionB
+    }
 
 }
 
